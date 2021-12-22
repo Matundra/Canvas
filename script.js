f88032319f3685d06deb3b3e75ffb2767c3b1380
Matundra <64801710+Matundra@users.noreply.github.com>
2021-12-22 12:01:57 -0500

    Version 3, Onclick multiple particles, minimize

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ const mouse = {
 canvas.addEventListener('click', function(event){
     mouse.x = event.x;
     mouse.y = event.y;
+    for (let i = 0; i < 10; i++){
+        particlesArray.push(new Particle());
+    }
 });
 
 canvas.addEventListener('mousemove', function(event){
@@ -24,36 +27,43 @@ canvas.addEventListener('mousemove', function(event){
 
 class Particle {
     constructor (){
-        //this.x = mouse.x;
-        //this.y =mouse.y;
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 5 + 1;
+        this.x = mouse.x;
+        this.y =mouse.y;
+        //this.x = Math.random() * canvas.width;
+        //this.y = Math.random() * canvas.height;
+        //Pour randomise les particles
+        this.size = Math.random() * 15 + 1;
         this.speedX = Math.random() * 3  - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
     }
     update(){
         this.x += this.speedX;
         this.y += this.speedY;
+        if (this.size > 0.2) this.size -= 0.1;//Shrink down this.size
     }
     draw(){
         ctx.fillStyle = 'brown';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fill();
     }
 }
-function init(){
+/*function init(){
     for (let i = 0; i < 100; i++){
         particlesArray.push(new Particle());
     }
 }
 init();
-
+*/
+//particle random 
 function handleParticles(){
     for (let i = 0; i < particlesArray.length; i++){
         particlesArray[i].update();
         particlesArray [i].draw();
+        if (particlesArray[i].size <= 0.3){
+        particlesArray.splice(i, 1);
+        i--;
+        }
     }
 }
 
